06ba68c92b92696c2c3e91003a77db6bf8044c82
m52325732 <m52325732@163.com>
2018-05-29 20:44:06 +0800

    5-29

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        effect : 'coverflow',
+        slidesPerView: 1.4,
+        centeredSlides: true,
+        initialSlide:4,
+
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+
+    })
+
+
+
+
+
+
+
+
+
+})
